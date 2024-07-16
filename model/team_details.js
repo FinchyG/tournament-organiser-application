52e1457bfdf67f3f9ee57339c0f881b8b37cc113
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2024-07-16 08:22:31 +0100

    files created for team details

---
+++ model/team_details.js
@@ -0,0 +1,11 @@
+const tournament_details = {
+    "message": "success",
+    "data" : [
+        {
+            "tournament_name": "tournament1",
+            "tournament_date": "2025-06-01",
+            "location_name": "location1",
+            "location_postcode": "postcode1" 
+        }
+    ]
+}
