da9b162bd14fdb6dfbb8c0f8a969d684e22f21fe
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2024-07-28 17:27:33 +0100

    home ground tables insertion references altered to -1

--- DOM_helpers/h_g_d.js
+++ DOM_helpers/h_g_d.js
@@ -18,56 +18,56 @@ function create_home_ground_table(i, home_ground_details_data) {
     let h_g_d_postcode = home_ground_details_data[i].home_ground_postcode;
     
     // create table element and insert into page_content div
-    const new_table = document.createElement("table");
-    document.getElementById("page_content").appendChild(new_table);
-    new_table.setAttribute("id", "table" + h_g_d_id);
+    const h_g_d_table = document.createElement("table");
+    document.getElementById("page_content").appendChild(h_g_d_table);
+    h_g_d_table.setAttribute("id", "table" + h_g_d_id);
 
     // add row for a table header to separate tables
-    const new_row0 = new_table.insertRow(0);
-    const new_cell0 = new_row0.insertCell(0);
+    const header_row = h_g_d_table.insertRow(-1);
+    const header_cell = header_row.insertCell(-1);
     const cell_header = "<p><u>home ground " + (i + 1) + "</u></p>";
-    new_cell0.innerHTML = cell_header;
+    header_cell.innerHTML = cell_header;
 
     // add row for home ground name display / input
-    const new_row1 = new_table.insertRow(1);
-    const new_cell1 = new_row1.insertCell(0);
-    new_cell1.innerHTML = "<input type='text' id='new_cell1' readonly />";
-    document.getElementById("new_cell1")
+    const h_g_name_row = h_g_d_table.insertRow(-1);
+    const h_g_name_cell = h_g_name_row.insertCell(-1);
+    h_g_name_cell.innerHTML = "<input type='text' id='h_g_name_cell' readonly />";
+    document.getElementById("h_g_name_cell")
         .setAttribute("id", `home_ground_name${h_g_d_id}`);
     document.getElementById(`home_ground_name${h_g_d_id}`)
         .setAttribute("value", `${h_g_d_name}`);
 
     // add row for home ground postcode display / input
-    const new_row2 = new_table.insertRow(2);
-    const new_cell2 = new_row2.insertCell(0);
-    new_cell2.innerHTML = "<input type='text' id='new_cell2' readonly />";
-    document.getElementById("new_cell2")
+    const h_g_h_g_postcode_row = h_g_d_table.insertRow(-1);
+    const h_g_h_g_postcode_cell = h_g_h_g_postcode_row.insertCell(-1);
+    h_g_h_g_postcode_cell.innerHTML = "<input type='text' id='h_g_h_g_postcode_cell' readonly />";
+    document.getElementById("h_g_h_g_postcode_cell")
         .setAttribute("id", `home_ground_postcode${h_g_d_id}`);
     document.getElementById(`home_ground_postcode${h_g_d_id}`)
         .setAttribute("value", `${h_g_d_postcode}`);
     
     // add row for edit button
-    const new_row3 = new_table.insertRow(3);
-    const new_cell3 = new_row3.insertCell(0);
-    new_cell3.innerHTML = "<button class='edit_button' id='new_cell3'" +
+    const edit_button_row = h_g_d_table.insertRow(-1);
+    const edit_button_cell = edit_button_row.insertCell(-1);
+    edit_button_cell.innerHTML = "<button class='edit_button' id='edit_button_cell'" +
         "onclick='edit_home_ground_details(this.id)'>edit</button>";
-    document.getElementById("new_cell3")
+    document.getElementById("edit_button_cell")
         .setAttribute("id", `edit_button${h_g_d_id}`);
 
     // add row for save button
-    const new_row4 = new_table.insertRow(4);
-    const new_cell4 = new_row4.insertCell(0);
-    new_cell4.innerHTML = "<button class='save_button' id='new_cell4'" +
+    const save_button_row = h_g_d_table.insertRow(-1);
+    const save_button_cell = save_button_row.insertCell(-1);
+    save_button_cell.innerHTML = "<button class='save_button' id='save_button_cell'" +
         "onclick='controller.save_home_ground_details(this.id)''>save</button>";
-    document.getElementById("new_cell4")
+    document.getElementById("save_button_cell")
         .setAttribute("id", `save_button${h_g_d_id}`);
 
     // add row for delete button
-    const new_row5 = new_table.insertRow(5);
-    const new_cell5 = new_row5.insertCell(0);
-    new_cell5.innerHTML = "<button class='delete_button' id='new_cell5'" +
+    const delete_button_row = h_g_d_table.insertRow(-1);
+    const delete_button_cell = delete_button_row.insertCell(-1);
+    delete_button_cell.innerHTML = "<button class='delete_button' id='delete_button_cell'" +
         "onclick='controller.delete_home_ground_details(this.id)''>delete</button>";
-    document.getElementById("new_cell5")
+    document.getElementById("delete_button_cell")
         .setAttribute("id", `delete_button${h_g_d_id}`);    
 }
 
@@ -88,58 +88,58 @@ function create_home_ground_data_entry_table() {
         document.getElementById("page_content").childElementCount;
 
     // create table element for insertion of new data
-    const new_table = document.createElement("table");
-    document.getElementById("page_content").appendChild(new_table);
-    new_table.setAttribute("id", "table" + new_data_element_id);
+    const h_g_d_table = document.createElement("table");
+    document.getElementById("page_content").appendChild(h_g_d_table);
+    h_g_d_table.setAttribute("id", "table" + new_data_element_id);
 
     // add row for a table header to separate tables
-    const new_row0 = new_table.insertRow(0);
-    const new_cell0 = new_row0.insertCell(0);
+    const header_row = h_g_d_table.insertRow(-1);
+    const header_cell = header_row.insertCell(-1);
     const cell_header =
         "<p><u>home ground " + (num_displayed_elements + 1) + "</u></p>";
-    new_cell0.innerHTML = cell_header;
+    header_cell.innerHTML = cell_header;
 
     // add row for home ground name display / input
-    const new_row1 = new_table.insertRow(1);
-    const new_cell1 = new_row1.insertCell(0);
-    new_cell1.innerHTML = "<input type='text' id='new_cell1' />";
-    document.getElementById("new_cell1")
+    const h_g_name_row = h_g_d_table.insertRow(-1);
+    const h_g_name_cell = h_g_name_row.insertCell(-1);
+    h_g_name_cell.innerHTML = "<input type='text' id='h_g_name_cell' />";
+    document.getElementById("h_g_name_cell")
         .setAttribute("id", `home_ground_name${new_data_element_id}`);
     document.getElementById(`home_ground_name${new_data_element_id}`)
         .setAttribute("value", "enter home ground name");
 
     // add row for home ground postcode display / input
-    const new_row2 = new_table.insertRow(2);
-    const new_cell2 = new_row2.insertCell(0);
-    new_cell2.innerHTML = "<input type='text' id='new_cell2' />";
-    document.getElementById("new_cell2")
+    const h_g_h_g_postcode_row = h_g_d_table.insertRow(-1);
+    const h_g_h_g_postcode_cell = h_g_h_g_postcode_row.insertCell(-1);
+    h_g_h_g_postcode_cell.innerHTML = "<input type='text' id='h_g_h_g_postcode_cell' />";
+    document.getElementById("h_g_h_g_postcode_cell")
         .setAttribute("id", `home_ground_postcode${new_data_element_id}`);
     document.getElementById(`home_ground_postcode${new_data_element_id}`)
         .setAttribute("value", "enter home ground postcode");
     
     // add row for edit button
-    const new_row3 = new_table.insertRow(3);
-    const new_cell3 = new_row3.insertCell(0);
-    new_cell3.innerHTML =
-        "<button class='edit_button' id='new_cell3'" +
+    const edit_button_row = h_g_d_table.insertRow(-1);
+    const edit_button_cell = edit_button_row.insertCell(-1);
+    edit_button_cell.innerHTML =
+        "<button class='edit_button' id='edit_button_cell'" +
         "onclick='edit_home_ground_details(this.id)'>edit</button>";
-    document.getElementById("new_cell3")
+    document.getElementById("edit_button_cell")
         .setAttribute("id", `edit_button${new_data_element_id}`);
 
     // add row for save button
-    const new_row4 = new_table.insertRow(4);
-    const new_cell4 = new_row4.insertCell(0);
-    new_cell4.innerHTML = "<button class='save_button' id='new_cell4'" +
+    const save_button_row = h_g_d_table.insertRow(-1);
+    const save_button_cell = save_button_row.insertCell(-1);
+    save_button_cell.innerHTML = "<button class='save_button' id='save_button_cell'" +
         "onclick='controller.save_home_ground_details(this.id)''>save</button>"
-    document.getElementById("new_cell4")
+    document.getElementById("save_button_cell")
         .setAttribute("id", `save_button${new_data_element_id}`);
 
     // add row for delete button
-    const new_row5 = new_table.insertRow(5);
-    const new_cell5 = new_row5.insertCell(0);
-    new_cell5.innerHTML = "<button class='delete_button' id='new_cell5'" +
+    const delete_button_row = h_g_d_table.insertRow(-1);
+    const delete_button_cell = delete_button_row.insertCell(-1);
+    delete_button_cell.innerHTML = "<button class='delete_button' id='delete_button_cell'" +
         "onclick='controller.delete_home_ground_details(this.id)''>delete</button>"
-    document.getElementById("new_cell5")
+    document.getElementById("delete_button_cell")
         .setAttribute("id", `delete_button${new_data_element_id}`);    
 
     // move user to bottom of page where new data entry table will appear
